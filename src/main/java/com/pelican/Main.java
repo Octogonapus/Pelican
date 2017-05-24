93ac6a68f798cee45c0828fc677fff9329aff96d
Octogonapus <firey45@gmail.com>
2017-05-24 02:19:30 -0400

    Initial commit

---
+++ src/main/java/com/pelican/Main.java
@@ -0,0 +1,7 @@
+package com.pelican;
+
+/**
+ * Created by Ryan on 5/24/2017.
+ */
+public class Main {
+}
---
+++ src/main/java/com/pelican/RenderingEngine.java
@@ -0,0 +1,8 @@
+package com.pelican;
+
+/**
+ * @author Ryan Benasutti
+ * @since 05-24-2017
+ */
+public class RenderingEngine {
+}
